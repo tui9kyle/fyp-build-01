27e9e5ef2817b1c0df9a4ae76f13b5359c0b3c39
tui9kyle <clf.kyle@gmail.com>
2022-02-09 23:07:12 +0800

    add binary search utility

--- src/library/tldp.js
+++ src/library/tldp.js
@@ -9,6 +9,19 @@ export class TldpUtilities {
             );
         } else return 1 / (k - 1 + Math.pow(Math.E, epsilon / 2));
     }
+
+    static BinarySearch(arr, target) {
+        let left = 0;
+        let right = arr.length;
+        let middle = 0;
+        while (left <= right) {
+            middle = Math.floor((left + right) / 2.0);
+            if (arr[middle] < target) left = middle + 1;
+            else if (arr[middle > target]) right = middle - 1;
+            else return middle;
+            return -1;
+        }
+    }
 }
 
 export class Tldp {
@@ -76,6 +89,74 @@ export class Tldp {
 
     // Threshold Mechanism
     static ThresholdMechanism(data, k, c0) {
+        let dataPerturbed = [];
+        let debugArr = [];
+
+        for (let i = 0; i < data.length; i++) {
+            // count the number of 0s in {x[i], x[i+1], ..., x[i+k-1]}
+            let c = 0;
+            for (let j = i; j < i + k; j++) {
+                if (dataPerturbed[j] == null) c++;
+            }
+            console.log(dataPerturbed);
+            console.log(c);
+            console.log(c > c0);
+
+            if (c > c0) {
+                // randomly select an index l from {i <= j < i+k}
+                // and dataPerturbed[j] == 0
+
+                let p = Math.random();
+                let idx = Math.floor(p * k + i);
+                console.log(p);
+                console.log(idx - i);
+                console.log(idx);
+
+                for (let j = i; j <= idx; j++) {
+                    if (dataPerturbed[j] != null) {
+                        idx++;
+                    }
+                }
+                console.log(idx);
+                dataPerturbed[idx] = data[i];
+            } else if (dataPerturbed[i] == null) {
+                console.log(i);
+                dataPerturbed[i] = data[i];
+            } else {
+                console.log(i);
+                let p = Math.random();
+                let idx = Math.floor(p * (k - 1) + (i + 1));
+                console.log(p);
+                console.log(idx);
+
+                for (let j = i; j <= idx; j++) {
+                    if (dataPerturbed[j] != null) {
+                        idx++;
+                    }
+                }
+                console.log(idx);
+                dataPerturbed[idx] = data[i];
+            }
+        }
+
+        let dataPerturbedFilled = dataPerturbed;
+
+        for (let i = 0; i < dataPerturbed.length; i++) {
+            if (!dataPerturbed[i]) dataPerturbedFilled[i] = "-";
+            else {
+                dataPerturbedFilled[i] = dataPerturbed[i];
+            }
+        }
+        console.log(dataPerturbed.length);
+        return {
+            result: dataPerturbed,
+            debugArr,
+            resultFilled: dataPerturbedFilled,
+        };
+    }
+
+    // (Extended) Threshold Mechanism
+    static ExtendedThresholdMechanism(data, k, epsilon) {
         let dataPerturbed = [];
         let debugArr = [];
 
